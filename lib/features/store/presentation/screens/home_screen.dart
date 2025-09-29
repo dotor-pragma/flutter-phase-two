3e028cf1f833a61ca96df2d347d834e62b105329
Alejandro Dotor <diego.dotor.ext@essity.com>
2025-09-29 09:26:56 -0500

    feat: add app bar and product list view to home screen

--- lib/features/store/presentation/screens/home_screen.dart
+++ lib/features/store/presentation/screens/home_screen.dart
@@ -10,9 +10,19 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final storeProvider = context.watch<StoreProvider>();
     return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
       body: storeProvider.isLoading
           ? const Center(child: CircularProgressIndicator(strokeWidth: 2))
-          : Center(child: Text('Home ${storeProvider.products.length}')),
+          : ListView.builder(
+              itemCount: storeProvider.products.length,
+              itemBuilder: (context, index) {
+                final product = storeProvider.products[index];
+                return ListTile(
+                  title: Text(product.title),
+                  subtitle: Text(product.price.toString()),
+                );
+              },
+            ),
     );
   }
 }
